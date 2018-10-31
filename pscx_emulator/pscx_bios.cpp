3536fdd3e49d10b4aee56d642eadcbe0d2612338
bibliophagist <greedthisissad@gmail.com>
2018-10-31 09:36:40 +0300

    lr2

--- pscx_emulator/pscx_bios.cpp
+++ pscx_emulator/pscx_bios.cpp
@@ -21,34 +21,28 @@
 //    } else {
 //       Err(Error::new(ErrorKind::InvalidInput,
 //                      "Invalid_BIOS_size"))
-//    } 
-//--------------------------------------------------
+//    }
+//------------------------------------------------
 Bios::BiosState Bios::loadBios(std::string path)
 {
-	uint32_t offset;
-	std::ifstream file(path, std::ios::binary);
+	std::basic_ifstream<uint8_t> biosFile(path, std::ios::in | std::ios::binary);
 
-	if (file.is_open()) {
-
-		std::vector<uint8_t> buffer((
-			std::istreambuf_iterator<char>(file)),
-			(std::istreambuf_iterator<char>()));
-	
-		if (buffer.size()==512*1024) {
-			m_data = buffer;
-			return BIOS_STATE_SUCCESS;
-		}
-		else
-		{
-			return BIOS_STATE_INVALID_BIOS_SIZE;
-		}
-	}
-	else {
+	if (!biosFile.good())
 		return BIOS_STATE_INCORRECT_FILENAME;
-	}
+
+	const uint32_t biosSize = 512 * 1024; // 512 kb
+
+	// Load the BIOS
+	m_data.insert(m_data.begin(), std::istreambuf_iterator<uint8_t>(biosFile), std::istreambuf_iterator<uint8_t>());
+
+	biosFile.close();
+
+	if (m_data.size() != biosSize)
+		return BIOS_STATE_INVALID_BIOS_SIZE;
+	return BIOS_STATE_SUCCESS;
 }
 
-uint32_t Bios::load32(uint32_t offset)
+uint32_t Bios::load32(uint32_t offset) const
 {
 	uint32_t b0 = m_data[offset + 0];
 	uint32_t b1 = m_data[offset + 1];
@@ -56,4 +50,9 @@ uint32_t Bios::load32(uint32_t offset)
 	uint32_t b3 = m_data[offset + 3];
 
 	return b0 | (b1 << 8) | (b2 << 16) | (b3 << 24);
+}
+
+uint8_t Bios::load8(uint32_t offset) const
+{
+	return m_data[offset];
 }
